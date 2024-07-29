449eee0a4cc0e3086002239ecb7d921d0cdbdbff
sergiifartushnyi <sergiyfartyshnyy@gmail.com>
2024-07-29 11:36:13 +0300

    д.з.3.2

---
+++ h.w.3.2.py
@@ -0,0 +1,27 @@
+a = [15,4,5,10]
+
+a.pop()
+
+a.insert(0,10)
+
+print(a)
+
+a = [1]
+
+a.pop()
+
+a.insert(0,1)
+
+print(a)
+
+a =[]
+
+print(a)
+
+a = [15,4,5,10,8]
+
+a.pop()
+
+a.insert(0,8)
+
+print(a)
